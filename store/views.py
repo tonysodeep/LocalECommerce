862fc3d1239d51542029b4e7d7d09d0ae923b7f6
tonysodeep <hynguyen2727@gmail.com>
2025-02-16 16:44:57 +0700

    template out and create about page

--- store/views.py
+++ store/views.py
@@ -7,3 +7,7 @@ def home(request):
     product_list = Product.objects.all()
     context = {'product_list': product_list}
     return render(request, 'store/index.html', context)
+
+
+def about(request):
+    return render(request, 'store/about.html', {})
