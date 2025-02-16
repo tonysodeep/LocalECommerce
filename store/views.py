7f43e8e5168a39b23beb586f234eef544c4dad13
tonysodeep <hynguyen2727@gmail.com>
2025-02-16 15:31:10 +0700

    changed category table mapped product to view

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def home(request):
-    return render(request, 'store/index.html', {})
+    product_list = Product.objects.all()
+    context = {'product_list': product_list}
+    return render(request, 'store/index.html', context)
