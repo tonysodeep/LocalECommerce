59a76624fbf0eb8d34795c69af04c20ee4d726db
tonysodeep <hynguyen2727@gmail.com>
2025-02-18 19:23:34 +0700

    added authenticate feature: login logout authen message

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about-us/', views.about, name='about')
+    path('about-us/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
@@ -11,3 +13,26 @@ def home(request):
 
 def about(request):
     return render(request, 'store/about.html', {})
+
+
+def login_user(request):
+    #check casesensitive
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, message="You have been login")
+            return redirect('home')
+        else:
+            messages.success(request, message="There a error please try again")
+            return redirect('login')
+    else:
+        return render(request, 'store/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, message="you have be logout")
+    return redirect('home')
