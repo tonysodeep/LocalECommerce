e63f2803a46771b2941623d89102964a24ddf9c1
tonysodeep <hynguyen2727@gmail.com>
2025-02-21 21:53:15 +0700

    restructure template folder

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def cart_summary(request):
+    return render(request, 'cart_summary.html', {})
+
+
+def cart_add(request):
+    return
+
+
+def cart_update(request):
+    return
+
+
+def cart_delete(request):
+    return
